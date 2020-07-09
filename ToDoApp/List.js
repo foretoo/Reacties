85ecc3faf87e34bbd1015b5c43cabc4f438017f0
Serg Borovikov <serg.there@gmail.com>
2020-07-09 18:45:58 +0300

    initiate editable items feature

--- ToDoApp/List.js
+++ ToDoApp/List.js
@@ -4,7 +4,7 @@ function List(props) {
   const items = props.items.map((e, i) => {
     return r("section", { className: "item", key: i }, [
       r("div", { className: "done" }),
-      r("div", { className: "item-text" }, e),
+      r("div", { className: "item-text", contenteditable: "true" }, r("div", null, e)),
       r("div", { className: "delete" })
     ])
   })
