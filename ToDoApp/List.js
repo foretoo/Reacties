38ce05e4abe08142cc4640366b2f69c0c20e2caa
Serg Borovikov <serg.there@gmail.com>
2020-07-09 18:58:03 +0300

    added Item component

--- ToDoApp/List.js
+++ ToDoApp/List.js
@@ -2,12 +2,19 @@
 function List(props) {
 
   const items = props.items.map((e, i) => {
-    return r("section", { className: "item", key: i }, [
-      r("div", { className: "done" }),
-      r("div", { className: "item-text", contenteditable: "true" }, r("div", null, e)),
-      r("div", { className: "delete" })
-    ])
+    return r(Item, { text: e, id: i})
   })
 
   return r("main", null, items);
+}
+
+
+
+function Item(props) {
+
+  return r("section", { className: "item", key: props.id }, [
+    r("div", { className: "done" }),
+    r("div", { className: "item-text", contenteditable: "true" }, r("div", null, props.text)),
+    r("div", { className: "delete" })
+  ])
 }
