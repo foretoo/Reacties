1262371dec48f41d7464c40d80ffeacafc1e9bd6
Serg Borovikov <serg.there@gmail.com>
2020-07-09 18:41:19 +0300

    improve styles and some DOM changes

--- ToDoApp/List.js
+++ ToDoApp/List.js
@@ -2,7 +2,11 @@
 function List(props) {
 
   const items = props.items.map((e, i) => {
-    return r("section", {key: i}, r("div", null, e))
+    return r("section", { className: "item", key: i }, [
+      r("div", { className: "done" }),
+      r("div", { className: "item-text" }, e),
+      r("div", { className: "delete" })
+    ])
   })
 
   return r("main", null, items);
--- ToDoApp/Nav.js
+++ ToDoApp/Nav.js
@@ -1,7 +1,7 @@
 function Nav() {
   return r("nav", null, [
-    r("input", {placeholder: "search"}),
-    r("ul", null, [
+    r("input", { className: "search", placeholder: "search" }),
+    r("ul", { className: "filter" }, [
       r("li", null, "All"),
       r("li", null, "Active"),
       r("li", null, "Done")
