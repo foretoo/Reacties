dd4b60b28c90ac5903f6347a7a40ccd69d78f54e
Serg Borovikov <serg.there@gmail.com>
2020-07-09 14:24:38 +0300

    styled, search panel and filter added

--- ToDoApp/List.js
+++ ToDoApp/List.js
@@ -1,8 +1,8 @@
 
 function List(props) {
 
-  const items = props.items.map(e => {
-    return r("div", null, e)
+  const items = props.items.map((e, i) => {
+    return r("section", {key: i}, r("div", null, e))
   })
 
   return r("main", null, items);
--- ToDoApp/Nav.js
+++ ToDoApp/Nav.js
@@ -1,3 +1,10 @@
 function Nav() {
-  return r("nav", null, "Nav");
+  return r("nav", null, [
+    r("input", {placeholder: "search"}),
+    r("ul", null, [
+      r("li", null, "All"),
+      r("li", null, "Active"),
+      r("li", null, "Done")
+    ])
+  ])
 }
