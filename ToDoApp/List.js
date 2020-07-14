adb123b7c71acd98d27374e92a8688fdebe90ab0
Serg Borovikov <serg.there@gmail.com>
2020-07-14 16:06:12 +0300

    merging editableItem branch

--- ToDoApp/List.js
+++ ToDoApp/List.js
@@ -2,13 +2,16 @@
 const List = ({ items, handleChange }) => {
 
   const itemList = items.map((el, i) => {
-    return r("section", { className: "item", key: el.id },
-      r(ContentEditable, {
-        className: "item-text",
-        innerRef: el.ref,
-        html: el.html,
-        onChange: e => handleChange(e.target, el.id)
-      }),
+    return r("section", { className: "item", key: el.id }, [
+        r("div", { className: "done" }),
+        r(ContentEditable, {
+          className: "item-text",
+          innerRef: el.ref,
+          html: el.html,
+          onChange: e => handleChange(e.target, el.id)
+        }),
+        r("div", { className: "delete" })
+      ]
     )
   })
 
