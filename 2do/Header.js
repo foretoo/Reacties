d4260753f7532bbd82a5de163eda7d8f6faec245
Ecospell <foretoo@machine.local>
2020-08-05 16:54:23 +0300

    header checks a date every minute

--- 2do/Header.js
+++ 2do/Header.js
@@ -1,11 +1,20 @@
 const Header = React.memo(() => {
-  const date = new Date()
+  const [date, setDate] = React.useState(new Date)
+
   const weekDay = date.toLocaleDateString(document.documentElement.lang, {weekday: 'long'})
   const monthDay = date.toLocaleDateString(document.documentElement.lang, {month: 'long', day: 'numeric'})
 
+  const interval = setInterval(() => {
+    const now = new Date()
+    if (now.getDay() !== date.getDay()) {
+      setDate(now)
+      clearInterval(interval)
+    }
+  }, 60000)
+
   return r("header", null, [
-    r("div", { className: "weekDay" }, weekDay),
     r("div", { className: "monthDay" }, monthDay),
+    r("div", { className: "weekDay" }, weekDay),
   ])
 })
 
