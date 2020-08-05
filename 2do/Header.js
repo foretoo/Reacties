21df71defd9f9d446d74738c22e57a7aadea90ea
Ecospell <foretoo@machine.local>
2020-08-05 14:29:54 +0300

    setting header

--- 2do/Header.js
+++ 2do/Header.js
@@ -1,3 +1,10 @@
 const Header = () => {
-  return r("header", null, "Header");
+  const date = new Date()
+  const weekDay = date.toLocaleDateString(document.documentElement.lang, {weekday: 'long'})
+  const monthDay = date.toLocaleDateString(document.documentElement.lang, {month: 'long', day: 'numeric'})
+
+  return r("header", null, [
+    r("div", { className: "weekDay" }, weekDay),
+    r("div", { className: "monthDay" }, monthDay),
+  ])
 }
