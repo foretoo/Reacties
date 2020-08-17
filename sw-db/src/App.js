4f8fa01e922b5704a061d873a93ade9e43d77003
Borovikov Sergey <serg.there@gmail.com>
2020-08-17 07:41:56 +0300

    rename index.js to App.js

---
+++ sw-db/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+
+class App extends Component {
+  render() {
+    return <div>Star Wars Data Base</div>
+  }
+}
+
+ReactDOM.render(<App />, window.root)
--- sw-db/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-
-const App = () => {
-  return <div>Star Wars Data Base</div>
-}
-
-ReactDOM.render(<App />, window.root)
