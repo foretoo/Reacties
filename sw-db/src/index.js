f07ab500884e125429c4641145c45bee14003b3f
Borovikov Sergey <serg.there@gmail.com>
2020-08-17 04:26:00 +0300

    Hello SW

--- sw-db/src/index.js
+++ sw-db/src/index.js
@@ -1,1 +1,8 @@
-console.log("working");
+import React from "react"
+import ReactDOM from "react-dom"
+
+const App = () => {
+  return <div>Star Wars Data Base</div>
+}
+
+ReactDOM.render(<App />, window.root)
