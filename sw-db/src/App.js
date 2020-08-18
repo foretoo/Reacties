5aba91a7ee58fc3fe239a04ff48208b35f4de731
Borovikov Sergey <serg.there@gmail.com>
2020-08-18 19:28:36 +0300

    init async fc

--- sw-db/src/App.js
+++ sw-db/src/App.js
@@ -1,5 +1,13 @@
 import React, {Component} from "react"
 
+const getResource = async url => {
+  const res = await fetch(url)
+  const body = await res.json()
+  return body
+}
+
+getResource("https://swapi.dev/api/people/1").then(res => console.log(res))
+
 export default class App extends Component {
   render() {
     return <div>Star Wars Data Base</div>
