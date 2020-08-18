5564cc16cf884c910d1619a7d244cc25b4c9434a
Borovikov Sergey <serg.there@gmail.com>
2020-08-18 21:32:51 +0300

    init app structure

--- sw-db/src/components/app/app.js
+++ sw-db/src/components/app/app.js
@@ -1,8 +1,20 @@
 import React, {Component} from "react"
 import "./app.css"
 
+import Header from "../header"
+import Random from "../random"
+import List from "../list"
+import Details from "../details"
+
 export default class App extends Component {
   render() {
-    return <div>App</div>
+    return (
+      <>
+        <Header />
+        <Random />
+        <List />
+        <Details />
+      </>
+    )
   }
 }
