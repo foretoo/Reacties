db3be4440732938ad71ffcd0051f6a34005494b0
Borovikov Sergey <serg.there@gmail.com>
2020-08-20 03:25:06 +0300

    made it fetching random planet data

--- sw-db/src/components/random/random.js
+++ sw-db/src/components/random/random.js
@@ -1,17 +1,47 @@
 import React, {Component} from "react"
 import "./random.css"
+import SwapiService from "../../services/SwapiService"
 
 export default class Random extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      id: null,
+      name: null,
+      population: null,
+      diameter: null,
+      rotationPeriod: null
+    }
+    this.updateData()
+  }
+
+  swapi = new SwapiService
+  updateData() {
+    const id = Math.floor(Math.random()*19) + 2
+    this.swapi.getPlanet(id)
+      .then(planet => {
+        this.setState({
+          id,
+          name: planet.name,
+          population: planet.population,
+          diameter: planet.diameter,
+          rotationPeriod: planet.rotation_period
+        })
+      })
+  }
+
   render() {
+    const { id, name, population, diameter, rotationPeriod } = this.state
     return (
       <section className="random">
-        <img />
+        <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}/>
         <div>
-          <h2>Random Name</h2>
+          <h2>{name}</h2>
           <ul>
-            <li>1 item</li>
-            <li>2 item</li>
-            <li>3 item</li>
+            <li>population: {population}</li>
+            <li>diameter: {diameter}</li>
+            <li>rotationPeriod: {rotationPeriod}</li>
           </ul>
         </div>
       </section>
