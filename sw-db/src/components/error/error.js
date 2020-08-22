cde75b09b86d928762c0f367a0d4ee56b0de23fd
Borovikov Sergey <serg.there@gmail.com>
2020-08-22 19:07:49 +0300

    added Error component

---
+++ sw-db/src/components/error/error.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./error.css"
+
+const Error = () => {
+  return (
+    <div className="error">
+      Something has gone uncommonly wrong,<br/>
+      but we`ve already sent droids to fix it!
+    </div>
+  )
+}
+
+export default Error
