1a9ab6f9edd69a5a53ab32cc19ebde8760fe0293
Borovikov Sergey <serg.there@gmail.com>
2020-08-24 23:31:30 +0300

    app now fetching data & throwing it to components

--- sw-db/src/components/app/app.js
+++ sw-db/src/components/app/app.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react"
+import SwapiService from "../../services/SwapiService"
 import "./app.css"
 
 import Header from "../header"
@@ -9,11 +10,64 @@ import Details from "../details"
 export default class App extends Component {
 
   state = {
-    page: "PLANETS"
+    page: "planets",
+    list: null,
+    hasList: false,
+    selectedId: null,
+    details: null,
+    hasDetails: false,
+  }
+
+  componentDidMount() {
+    this.getList(this.state.page)
+  }
+
+  swapi = new SwapiService
+  getList = (type) => {
+    if (type === "planets") {
+      this.swapi.getAllPlanets()
+        .then(list => {
+          this.setState({ list, hasList: true, selectedId: list[0].id })
+          this.getObject(type, list[0].id)
+        })
+    }
+    if (type === "people") {
+      this.swapi.getAllPeople()
+        .then(list => {
+          this.setState({ list, hasList: true, selectedId: list[0].id })
+          this.getObject(type, list[0].id)
+        })
+    }
+    if (type === "starships") {
+      this.swapi.getAllStarships()
+        .then(list => {
+          this.setState({ list, hasList: true, selectedId: list[0].id })
+          this.getObject(type, list[0].id)
+        })
+    }
+  }
+  getObject = (type, id) => {
+    if (type === "planets") {
+      this.swapi.getPlanet(id)
+        .then(planet => this.setState({ details: {...planet}, hasDetails: true }))
+    }
+    if (type === "people") {
+      this.swapi.getPerson(id)
+        .then(person => this.setState({ details: {...person}, hasDetails: true }))
+    }
+    if (type === "starships") {
+      this.swapi.getStarship(id)
+        .then(starship => this.setState({ details: {...starship}, hasDetails: true }))
+    }
   }
 
   handlePageChange = (page) => {
-    this.setState({ page: page })
+    this.setState({ page: page, hasList: false, selectedId: null })
+    this.getList(page)
+  }
+  handleSelected = (id) => {
+    this.setState({ selectedId: id })
+    this.getObject(this.state.page, id)
   }
 
   render() {
@@ -25,8 +79,16 @@ export default class App extends Component {
         />
         <Random />
         <main>
-          <List />
-          <Details />
+          <List
+            list={this.state.list}
+            hasList={this.state.hasList}
+            handleSelected ={this.handleSelected}
+          />
+          <Details
+            type={this.state.page}
+            details={this.state.details}
+            hasDetails={this.state.hasDetails}
+          />
         </main>
       </>
     )
