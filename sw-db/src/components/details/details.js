27683c33fc2649b8a10b81ddfa49180bcdb76637
Borovikov Sergey <serg.there@gmail.com>
2020-08-24 23:29:49 +0300

    details now working

--- sw-db/src/components/details/details.js
+++ sw-db/src/components/details/details.js
@@ -1,20 +1,28 @@
-import React, {Component} from "react"
+import React from "react"
+import Loader from "../loader"
 import "./details.css"
 
-export default class Details extends Component {
-  render() {
+const Details = (props) => {
+  if (!props.hasDetails) {
     return (
       <section className="details">
-        <img />
-        <div>
-          <h2>Name</h2>
-          <ul>
-            <li>1 item</li>
-            <li>2 item</li>
-            <li>3 item</li>
-          </ul>
-        </div>
+        <Loader />
       </section>
     )
   }
+  const items = Object.entries(props.details).map((v,i) => {
+    if (!i) return null
+    if (i === 1) return <li className="title" key={`${props.details.id}_${i}`}>{v[1]}</li>
+    return <li key={`${props.details.id}_${i}`}>{`${v[0]}: ${v[1]}`}</li>
+  })
+  const imgPath = props.type === "people" ? "characters" : props.type
+  return (
+    <section className="details">
+      <div className="img">
+        <img src={`https://starwars-visualguide.com/assets/img/${imgPath}/${props.details.id}.jpg`} />
+      </div>
+      <ul>{items}</ul>
+    </section>
+  )
 }
+export default Details
