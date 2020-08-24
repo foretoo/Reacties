77deb77f0f1a459e8a88fc4c4a1771cb3d1e7ee5
Borovikov Sergey <serg.there@gmail.com>
2020-08-24 17:37:48 +0300

    set navigation works

--- sw-db/src/components/app/app.js
+++ sw-db/src/components/app/app.js
@@ -7,10 +7,22 @@ import List from "../list"
 import Details from "../details"
 
 export default class App extends Component {
+
+  state = {
+    page: "PLANETS"
+  }
+
+  handlePageChange = (page) => {
+    this.setState({ page: page })
+  }
+
   render() {
     return (
       <>
-        <Header />
+        <Header
+          page={this.state.page}
+          handlePageChange={this.handlePageChange}
+        />
         <Random />
         <main>
           <List />
--- sw-db/src/components/header/header.js
+++ sw-db/src/components/header/header.js
@@ -1,17 +1,31 @@
-import React, {Component} from "react"
+import React from "react"
 import "./header.css"
 
-export default class Header extends Component {
-  render() {
-    return (
-      <header>
-        <h1>StarWars Database</h1>
-        <nav>
-          <div>People</div>
-          <div>Planets</div>
-          <div>Starships</div>
-        </nav>
-      </header>
-    )
-  }
+const Header = (props) => {
+
+  const { page, handlePageChange } = props
+  
+  return (
+    <header>
+      <h1>StarWars Database</h1>
+      <nav>
+        <div
+          className={page === "PEOPLE" ? "active" : null}
+          onClick={() => handlePageChange("PEOPLE")}>
+          People
+        </div>
+        <div
+          className={page === "PLANETS" ? "active" : null}
+          onClick={() => handlePageChange("PLANETS")}>
+          Planets
+        </div>
+        <div
+          className={page === "STARSHIPS" ? "active" : null}
+          onClick={() => handlePageChange("STARSHIPS")}>
+          Starships
+        </div>
+      </nav>
+    </header>
+  )
 }
+export default Header
