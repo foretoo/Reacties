5fd8a314bea54d29523b6cb6b946f0926e7c3aeb
Borovikov Sergey <serg.there@gmail.com>
2020-08-28 01:06:28 +0300

    component now fetching by itself. added comparison function to memo to prevent collisions on page change

--- sw-db/src/components/details/details.js
+++ sw-db/src/components/details/details.js
@@ -1,29 +1,41 @@
-import React, { memo } from "react"
+import React, { memo, useState, useEffect } from "react"
 import Loader from "../loader"
 import Image from "../image"
 import "./details.css"
 
-const Details = memo(({ type, details }) => {
+const shouldDetailsUpdate = (prevProps, nextProps) => {
+  if (!prevProps.id || !nextProps.id) return !true
+  if (prevProps.type === nextProps.type && prevProps.id !== nextProps.id) return !true
+  return !false
+}
 
+const Details = memo(({ type, id, getData }) => {
+
+  const [data, setData] = useState(null)
   let output
 
-  if (!details) output = <Loader/>
+  useEffect(() => {
+    id ? getData(type, id).then(data => setData(data)) : setData(null)
+  }, [type, id])
+
+  if (!data || !id) output = <Loader/>
   else {
-    const items = Object.entries(details).map((v,i) => {
+    const items = Object.entries(data).map((v,i) => {
       if (!i)       return null
-      if (i === 1)  return <li className="title" key={`${details.id}_${i}`}><h2>{v[1]}</h2></li>
-      return <li key={`${details.id}_${i}`}>{`${v[0]}: ${v[1]}`}</li>
+      if (i === 1)  return <li className="title" key={`${id}_${i}`}><h2>{v[1]}</h2></li>
+      return <li key={`${id}_${i}`}>{`${v[0]}: ${v[1]}`}</li>
     })
     output =
     <>
       <div className="img">
-        <Image type={type} id={details.id} />
+        <Image type={type} id={id} />
       </div>
       <ul>{items}</ul>
     </>
   }
 
   return <section className="details">{output}</section>
-})
+
+}, shouldDetailsUpdate)
 
 export default Details
