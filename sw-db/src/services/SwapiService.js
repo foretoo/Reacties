f81a8eda8226af565fcbfb228cad5cc72020f97c
Borovikov Sergey <serg.there@gmail.com>
2020-08-28 01:01:29 +0300

    fix: this issue

--- sw-db/src/services/SwapiService.js
+++ sw-db/src/services/SwapiService.js
@@ -2,7 +2,7 @@ export default class SwapiService {
 
   _apiBase = "https://swapi.dev/api"
 
-  async getResource(url) {
+  getResource = async (url) => {
     const res = await fetch(`${this._apiBase}${url}`)
     if (!res.ok) {
       throw new Error(`Could not fetch ${this._apiBase}${url}}, recieve ${res.status}`)
@@ -11,42 +11,42 @@ export default class SwapiService {
     return body
   }
 
-  async getAllPeople() {
+  getAllPeople = async () => {
     const res = await this.getResource("/people/")
     return res.results.map(this._transformPerson)
   }
 
-  async getPerson(id) {
+  getPerson = async (id) => {
     const person = await this.getResource(`/people/${id}/`)
     return this._transformPerson(person)
   }
 
-  async getAllPlanets() {
+  getAllPlanets = async () => {
     const res = await this.getResource("/planets/")
     return res.results.map(this._transformPlanet)
   }
 
-  async getPlanet(id) {
+  getPlanet = async (id) => {
     const planet = await this.getResource(`/planets/${id}/`)
     return this._transformPlanet(planet)
   }
 
-  async getAllStarships() {
+  getAllStarships = async () => {
     const res = await this.getResource("/starships/")
     return res.results.map(this._transformStarship)
   }
 
-  async getStarship(id) {
+  getStarship = async (id) => {
     const starship = await this.getResource(`/starships/${id}/`)
     return this._transformStarship(starship)
   }
 
-  _getId(item) {
+  _getId = (item) => {
     const regEx = /\/([0-9]*)\/$/
     return item.url.match(regEx)[1]
   }
 
-  _formatted(data) {
+  _formatted = (data) => {
     return isNaN(data) ? data : new Intl.NumberFormat("RU").format(data)
   }
 
