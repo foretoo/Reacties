60aede2b419c02b1fe3c149fd43180462333d1d8
Borovikov Sergey <serg.there@gmail.com>
2020-08-28 01:03:53 +0300

    component now fetching by itself. memo not needed

--- sw-db/src/components/list/list.js
+++ sw-db/src/components/list/list.js
@@ -1,10 +1,19 @@
-import React, { memo } from "react"
+import React, { useState, useEffect } from "react"
 import Loader from "../loader"
 import "./list.css"
 
-const List = memo(({ list, handleSelect }) => {
 
+const List = ({ type, getData, handleSelect }) => {
+
+  const [list, setList] = useState(null)
   let output
+
+  useEffect(() => {
+    getData(type).then(data => {
+      setList(data)
+      handleSelect(data[0].id)
+    })
+  }, [type])
 
   if (list) {
     output = list.map(({ id, name }) => {
@@ -18,6 +27,6 @@ const List = memo(({ list, handleSelect }) => {
   else output = <Loader />
 
   return <ul>{output}</ul>
-})
+}
 
 export default List
