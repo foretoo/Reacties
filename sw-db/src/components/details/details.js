36ce70e918e1631ecc4d50d9e85771c408d81ef9
Borovikov Sergey <serg.there@gmail.com>
2020-08-27 21:43:31 +0300

    got more readable

--- sw-db/src/components/details/details.js
+++ sw-db/src/components/details/details.js
@@ -3,29 +3,27 @@ import Loader from "../loader"
 import Image from "../image"
 import "./details.css"
 
-const Details = memo((props) => {
+const Details = memo(({ type, details }) => {
 
-  if (!props.hasDetails) {
-    return (
-      <section className="details">
-        <Loader style={{marginLeft: "20px"}}/>
-      </section>
-    )
-  }
+  let output
 
-  const items = Object.entries(props.details).map((v,i) => {
-    if (!i) return null
-    if (i === 1) return <li className="title" key={`${props.details.id}_${i}`}><h2>{v[1]}</h2></li>
-    return <li key={`${props.details.id}_${i}`}>{`${v[0]}: ${v[1]}`}</li>
-  })
-
-  return (
-    <section className="details">
+  if (!details) output = <Loader/>
+  else {
+    const items = Object.entries(details).map((v,i) => {
+      if (!i)       return null
+      if (i === 1)  return <li className="title" key={`${details.id}_${i}`}><h2>{v[1]}</h2></li>
+      return <li key={`${details.id}_${i}`}>{`${v[0]}: ${v[1]}`}</li>
+    })
+    output =
+    <>
       <div className="img">
-        <Image type={props.type} id={props.details.id} />
+        <Image type={type} id={details.id} />
       </div>
       <ul>{items}</ul>
-    </section>
-  )
+    </>
+  }
+
+  return <section className="details">{output}</section>
 })
+
 export default Details
