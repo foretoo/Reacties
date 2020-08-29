79f3c4398b5c2be512463ba6a763c85f967e7a88
Borovikov Sergey <serg.there@gmail.com>
2020-08-29 03:06:04 +0300

    threejs component added

---
+++ sw-db/src/threejs/ThreeScene.js
@@ -0,0 +1,57 @@
+import * as THREE from 'three'
+import img from "./star.png"
+
+export default function ThreeScene(sceneRef) {
+
+  const scene = new THREE.Scene()
+
+  const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000)
+    camera.position.z = 1
+    camera.rotation.x = Math.PI / 2
+
+  const renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    sceneRef.appendChild(renderer.domElement)
+
+  const starGeo = new THREE.Geometry()
+    for (let i = 0; i < 500; i++) {
+      const star = new THREE.Vector3(
+        Math.random() * 400 - 200,
+        Math.random() * 1200 - 600,
+        Math.random() * 400 - 200
+      )
+      star.velocity = 0
+      star.acceleration = 0.02
+      starGeo.vertices.push(star)
+    }
+
+  const sprite = new THREE.TextureLoader().load(img)
+  const starMaterial = new THREE.PointsMaterial({
+    color: 0xaaaaaa,
+    transparent: true,
+    fog: false,
+    size: 0.5,
+    map: sprite
+  })
+  const stars = new THREE.Points(starGeo, starMaterial)
+
+  scene.add(stars)
+
+  function animate() {
+    starGeo.vertices.forEach(star => {
+      star.velocity += star.acceleration
+      star.y -= star.velocity
+      if (star.y < -(100 + Math.random()*500)) {
+        star.y = 100 + Math.random()*500
+        star.velocity = 0
+      }
+    });
+    starGeo.verticesNeedUpdate = true
+    stars.rotation.y += 0.001
+
+    renderer.render(scene, camera)
+    requestAnimationFrame(animate)
+  }
+
+  animate()
+}
