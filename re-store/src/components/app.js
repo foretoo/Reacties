ea01dfcfcdc9abdaec7fe37ef37745d8b7bb7968
Ecospell <foretoo@machine.local>
2020-09-22 13:14:11 +0300

    init app.css

--- re-store/src/components/app.js
+++ re-store/src/components/app.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux'
 import { Context } from './context'
 import Loader from './loader'
 
+import './app.css'
+
 const App = ({ str }) => {
   const { cnx } = useContext(Context)
   return (
