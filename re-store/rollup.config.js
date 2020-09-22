846303b7daf3d583725ee40d0aca695b1d013718
Ecospell <foretoo@machine.local>
2020-09-22 04:14:12 +0300

    ~

--- re-store/rollup.config.js
+++ re-store/rollup.config.js
@@ -1,15 +1,16 @@
-import replace from '@rollup/plugin-replace'
 import alias from '@rollup/plugin-alias'
+import cleaner from 'rollup-plugin-cleaner'
+import replace from '@rollup/plugin-replace'
 import resolve from '@rollup/plugin-node-resolve'
-import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
+import babel from '@rollup/plugin-babel'
 import postcss from 'rollup-plugin-postcss'
 import html from 'rollup-plugin-generate-html-template'
 import { terser } from "rollup-plugin-terser"
 import serve from 'rollup-plugin-serve'
 import livereload from 'rollup-plugin-livereload'
 
-const mode = process.env.BUILD ? 'production' : 'development'
+const mode = process.env.PROD ? 'production' : 'development'
 
 export default {
   input: 'src/index.js',
@@ -18,27 +19,36 @@ export default {
     format: 'iife'
   },
   plugins: [
-    replace({ 'process.env.NODE_ENV': JSON.stringify(mode) }),
     alias({
       entries: [
         { find: 'react', replacement: 'preact/compat' },
         { find: 'react-dom', replacement: 'preact/compat' }
       ]
     }),
+    cleaner({
+      targets: ['build']
+    }),
+    replace({ 'process.env.NODE_ENV': JSON.stringify(mode) }),
     resolve(),
-    commonjs({ include: /node_modules/ }),
+    commonjs({
+      include: /node_modules/
+    }),
     babel({
       exclude: /node_modules/,
       babelHelpers: 'bundled'
     }),
-    postcss({
-      extract: true
-    }),
+    process.env.DEV && postcss({ extract: true }),
+    process.env.PROD && (
+      postcss({
+        extract: true,
+        minimize: true
+      })
+    ),
     html({
       template: 'src/index.html',
       target: 'index.html'
     }),
-    process.env.BUILD && terser(),
+    process.env.PROD && terser(),
     process.env.DEV && (
       serve({
         open: true,
