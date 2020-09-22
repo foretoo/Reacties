5dc731374be0b9047684a38ba7eacccdde0e6a11
Ecospell <foretoo@machine.local>
2020-09-22 14:10:25 +0300

    init separated store

--- re-store/src/index.js
+++ re-store/src/index.js
@@ -1,12 +1,11 @@
 import { h, render } from 'preact'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import reducer from './reducers/reducer'
 import { ContextProvider } from './components/context'
+import store from './store'
 import App from './components/app'
 import './index.css'
 
-const store = createStore(reducer)
+
 
 const HOApp = () => {
   return (
