611c53072dd71578d2c9c152e59d6e394ab035b1
Ecospell <foretoo@machine.local>
2020-09-22 19:35:44 +0300

    provided books[]

--- re-store/src/components/book-list.js
+++ re-store/src/components/book-list.js
@@ -1,9 +1,10 @@
 import { h } from 'preact'
+import { connect } from 'react-redux'
 import BookItem from './book-item'
 
 const BookList = ({ books }) => {
 
-  const list = books.map( book => <li key={book.id}><BookItem book /></li> )
+  const list = books.map( book => <li key={book.id}><BookItem book={book} /></li> )
 
   return (
     <ul>
@@ -11,4 +12,7 @@ const BookList = ({ books }) => {
     </ul>
   )
 }
-export default BookList
+
+const mapStateToProps = state => state
+
+export default connect(mapStateToProps)(BookList)
