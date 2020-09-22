6c12b2b8185c8607f90d4df57ed6573acc3e3f73
Ecospell <foretoo@machine.local>
2020-09-22 15:13:55 +0300

    clear

--- re-store/src/components/app.js
+++ re-store/src/components/app.js
@@ -1,22 +1,15 @@
 import { h } from 'preact'
-import { useContext } from 'preact/hooks'
-import { connect } from 'react-redux'
-import { Context } from './context'
 import ErrorCatcher from './error'
 import Loader from './loader'
 
 import './app.css'
 
-const App = ({ state }) => {
-  const { cnx } = useContext(Context)
+const App = () => {
   return (
     <ErrorCatcher>
-      {state}
       <Loader />
     </ErrorCatcher>
   )
 }
 
-const mapStateToProps = state => state
-
-export default connect(mapStateToProps)(App)
+export default App
