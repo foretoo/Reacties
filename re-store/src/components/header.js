0cf595f84901860a3a3cad24c3ce96f905bc8b2e
Ecospell <foretoo@machine.local>
2020-09-23 02:17:46 +0300

    :D

--- re-store/src/components/header.js
+++ re-store/src/components/header.js
@@ -5,7 +5,7 @@ import './css/header.css'
 const Header = () => {
   return (
     <nav class='navigation'>
-      <Link to='/'>8oo§+o</Link>
+      <Link to='/'>boo§+o</Link>
       <Link to='/cart'>Cart</Link>
     </nav>
   )
