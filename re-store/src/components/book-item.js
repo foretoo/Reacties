340c45a943ed722d4a9a160ade02245531f5c3d2
Ecospell <foretoo@machine.local>
2020-09-25 12:03:04 +0300

    modified

--- re-store/src/components/book-item.js
+++ re-store/src/components/book-item.js
@@ -5,10 +5,12 @@ import './css/book-item.css'
 const BookItem = ({ book }) => {
 
   const { title, author, imgName } = book
+  const angel = Math.floor(Math.random()*17 - 8)
+  const style = { transform: `rotate(${angel}deg)` }
 
   return (
     <li class='book-item'>
-      <div class='book-ellipse'></div>
+      <div style={style} class='book-ellipse'></div>
       <img class='book-img' src={images[imgName]} />
       <div class='book-info'>
         <p class='book-title'>{title}</p>
