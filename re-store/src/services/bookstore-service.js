fd1ad6638cd331907f7e3a5a39ace3140d958340
Ecospell <foretoo@machine.local>
2020-09-27 22:46:04 +0300

    added book price

--- re-store/src/services/bookstore-service.js
+++ re-store/src/services/bookstore-service.js
@@ -6,11 +6,13 @@ export default class BookstoreService {
         id: 1,
         title: 'Production-ready Microservices',
         author: 'Susan J. Fowler',
+        price: 129,
         imgName: 'prm' },
       {
         id: 2,
         title: 'Realese It!',
         author: 'Michael T. Nygard',
+        price: 71,
         imgName: 'rli' }
     ]
   }
