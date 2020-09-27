ad70fd256c8cb0a650d3738c6afbd52fb9c045dc
Ecospell <foretoo@machine.local>
2020-09-27 22:46:53 +0300

    added CartItem component

---
+++ re-store/src/components/cart-item.js
@@ -0,0 +1,19 @@
+import { h } from 'preact'
+import './css/cart-item.css'
+
+const CartItem = ({ book }) => {
+  return (
+    <li class='cart-item'>
+      <span class='cart-item-title'>{book.title}</span>
+      <div class='cart-item-count-container'>
+        <button>-</button>
+        <span class='cart-item-count'>{book.count}</span>
+        <button>+</button>
+      </div>
+      <span class='cart-item-price'>{book.price}£</span>
+      <button>delete</button>
+    </li>
+  )
+}
+
+export default CartItem
