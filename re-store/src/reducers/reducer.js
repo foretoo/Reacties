e88b7d6a7c124a3385a04cd5a717cd838856a5e1
Borovikov Sergey <serg.there@gmail.com>
2020-10-01 01:39:12 +0300

    refactored++[2]

--- re-store/src/reducers/reducer.js
+++ re-store/src/reducers/reducer.js
@@ -2,87 +2,93 @@ const initialState = {
   lib: {
     loading: true,
     books: [],
-    error: null,
+    error: null
   },
   cart: {
     books: [],
     num: 0,
-    sum: 0,
+    sum: 0
   }
 }
 
-const getCartBook = (state, id) => {
-  const idx = state.cart.books.findIndex(book => book.id === id)
-  const book = state.cart.books[idx] || null
-  return { book, idx }
-}
-
-const updatedCart = (state, book, idx, num) => {
-  const newCart = state.cart
-  book.count = book.count || 0
-  book.count += num
-
-  if (idx < 0) newCart.books.push(book)
-  else if (book.count === 0) newCart.books.splice(idx, 1)
-  else newCart.books.splice(idx, 1, book)
-
-  return {
-    ...state,
-    cart: {
-      books: newCart.books,
-      num: newCart.num + num,
-      sum: newCart.sum + book.price * num
-    }
-  }
-}
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-
+const updateLib = ({ type, payload }) => {
+  switch (type) {
     case 'CLEAR_BOOKS':
       return {
-        ...state,
         loading: true,
         books: [],
         error: null
       }
     case 'FETCH_BOOKS_SUCCES':
       return {
-        ...state,
         loading: false,
-        books: action.payload,
+        books: payload,
         error: null
       }
     case 'FETCH_BOOKS_FAILURE':
       return {
-        ...state,
         loading: false,
         books: [],
-        error: action.payload
+        error: payload
       }
+  }
+}
 
+const updateCart = (cart, action) => {
+
+  const getCartBook = (cart, id) => {
+    const idx = cart.books.findIndex(book => book.id === id)
+    const book = cart.books[idx] || null
+    return { book, idx }
+  }
+
+  const updatedCartList = (cart, book, idx, num) => {
+    const newCart = cart
+    book.count = book.count || 0
+    book.count += num
+    if (idx < 0) newCart.books.push(book)
+    else if (book.count === 0) newCart.books.splice(idx, 1)
+    else newCart.books.splice(idx, 1, book)
+    return {
+      books: newCart.books,
+      num: newCart.num + num,
+      sum: newCart.sum + book.price * num
+    }
+  }
+
+  switch (action.type) {
     case 'ADD_CART_BOOK': {
-      let { book, idx } = getCartBook(state, action.payload.id)
+      let { book, idx } = getCartBook(cart, action.payload.id)
       if (!book) book = action.payload
-      return updatedCart(state, book, idx, 1)
+      return updatedCartList(cart, book, idx, 1)
     }
-
     case 'INC_CART_BOOK': {
-      const { book, idx } = getCartBook(state, action.payload)
-      return updatedCart(state, book, idx, 1)
+      const { book, idx } = getCartBook(cart, action.payload)
+      return updatedCartList(cart, book, idx, 1)
     }
-
     case 'DEC_CART_BOOK': {
-      const { book, idx } = getCartBook(state, action.payload)
-      return updatedCart(state, book, idx, -1)
+      const { book, idx } = getCartBook(cart, action.payload)
+      return updatedCartList(cart, book, idx, -1)
     }
-
     case 'DEL_CART_BOOK': {
-      const { book, idx } = getCartBook(state, action.payload)
+      const { book, idx } = getCartBook(cart, action.payload)
       const count = -book.count
-      return updatedCart(state, book, idx, count)
+      return updatedCartList(cart, book, idx, count)
     }
+  }
+}
 
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'CLEAR_BOOKS':
+    case 'FETCH_BOOKS_SUCCES':
+    case 'FETCH_BOOKS_FAILURE':
+      return { ...state, lib: updateLib(action) }
+    case 'ADD_CART_BOOK':
+    case 'INC_CART_BOOK':
+    case 'DEC_CART_BOOK':
+    case 'DEL_CART_BOOK':
+      return { ...state, cart: updateCart(state.cart, action) }
     default:
       return state
   }
