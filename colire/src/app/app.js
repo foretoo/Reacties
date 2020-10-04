274847ac7ed68d8ce97aa2eee710d4cca7072a02
Ecospell <foretoo@yandex.ru>
2020-10-04 21:30:07 +0300

    configured routing

--- colire/src/app/app.js
+++ colire/src/app/app.js
@@ -1,11 +1,40 @@
-import { h } from 'preact'
+import { h, Fragment } from 'preact'
 import { useContext } from 'preact/hooks'
+import { Link, Route } from 'react-router-dom'
 import Palette from '../components/palette'
 import { Context } from './context'
+import './app.css'
 
 const App = () => {
+
   const { seedColors } = useContext(Context)
-  return <Palette {...seedColors[1]} />
+  const paletteLinks = [], palettePaths = []
+
+  for (const palette of seedColors) {
+    paletteLinks.push(
+      <Link to={palette.id} class='palette-box'>
+        <div class='palette-box-name'>{palette.paletteName}</div>
+      </Link>
+    )
+    palettePaths.push(
+      <Route path={`/${palette.id}`}>
+        <Palette {...palette}/>
+      </Route>
+    )
+  }
+
+  return (
+    <>
+      <Route path='/' exact>
+        <header class='home-header'></header>
+        <main class='home-main'>
+          {paletteLinks}
+        </main>
+        <footer class='home-footer'></footer>
+      </Route>
+      {palettePaths}
+    </>
+  )
 }
 
 export default App
