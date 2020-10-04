e9da4494109c54c533046cc429e82b6e3006fc4c
Ecospell <foretoo@yandex.ru>
2020-10-05 00:25:42 +0300

    add: color-box-ui

--- colire/src/components/color-box.js
+++ colire/src/components/color-box.js
@@ -4,7 +4,10 @@ import './css/color-box.css'
 const ColorBox = ({ name, color }) => {
   return (
     <div class='color-box' style={{ background: color }}>
-      {name}
+      <button class='color-box-button'>Copy</button>
+      <div class='color-box-info'>
+        <div class='color-box-info-name'>{name}</div>
+      </div>
     </div>
   )
 }
