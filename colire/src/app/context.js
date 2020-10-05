36970f3a50e64f295b34cc056a4f8eb206ce542a
Borovikov Sergey <serg.there@gmail.com>
2020-10-06 02:15:09 +0300

    add: reducer

--- colire/src/app/context.js
+++ colire/src/app/context.js
@@ -1,12 +1,36 @@
 import { h, createContext } from 'preact'
+import { useReducer } from 'preact/hooks'
 import seedColors from './seedColors'
 
 const Context = createContext()
 
+const initState = {
+  copy: false,
+  color: ''
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'COPY':
+      return {
+        copy: true,
+        color: action.payload
+      }
+    case 'ANIMATION_DONE':
+      return {
+        ...state,
+        copy: false
+      }
+    default:
+      return state
+  }
+}
+
 const ContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initState)
   return (
     <Context.Provider value={{
-      seedColors
+      seedColors, state, dispatch
     }}>
       {children}
     </Context.Provider>
