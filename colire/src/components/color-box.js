09cd6f896eab27e3722dade2161282be3f17da5b
Ecospell <foretoo@yandex.ru>
2020-10-05 03:25:56 +0300

    add: 'more' button

--- colire/src/components/color-box.js
+++ colire/src/components/color-box.js
@@ -4,9 +4,10 @@ import './css/color-box.css'
 const ColorBox = ({ name, color }) => {
   return (
     <div class='color-box' style={{ background: color }}>
-      <button class='color-box-button'>Copy</button>
+      <button class='color-box-button'>COPY</button>
       <div class='color-box-info'>
         <div class='color-box-info-name'>{color}</div>
+        <button class='color-box-info-more'>MORE</button>
       </div>
     </div>
   )
