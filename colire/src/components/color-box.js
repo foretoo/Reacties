e38b58643f765265ad56214aabbdd0608e682c9e
Borovikov Sergey <serg.there@gmail.com>
2020-10-05 18:23:18 +0300

    add: react-copy-to-clipboard module

--- colire/src/components/color-box.js
+++ colire/src/components/color-box.js
@@ -1,15 +1,18 @@
 import { h } from 'preact'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 import './css/color-box.css'
 
 const ColorBox = ({ name, color }) => {
   return (
-    <div class='color-box' style={{ background: color }}>
-      <button class='color-box-button'>COPY</button>
-      <div class='color-box-info'>
-        <div class='color-box-info-name'>{color}</div>
-        <button class='color-box-info-more'>MORE</button>
+    <CopyToClipboard text={color}>
+      <div class='color-box' style={{ background: color }}>
+        <button class='color-box-button'>COPY</button>
+        <div class='color-box-info'>
+          <div class='color-box-info-name'>{color}</div>
+          <button class='color-box-info-more'>MORE</button>
+        </div>
       </div>
-    </div>
+    </CopyToClipboard>
   )
 }
 
