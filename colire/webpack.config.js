c6af4992b9b98afde5a72f2f5c1f2655f2667216
Borovikov Sergey <serg.there@gmail.com>
2020-10-06 03:59:59 +0300

    add: conditional bundling

--- colire/webpack.config.js
+++ colire/webpack.config.js
@@ -3,53 +3,54 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const myIP = (process.platform === 'darwin' && '172.20.10.4') || (process.platform === 'win32' && '172.20.10.2') || 'localhost'
 const port = '8000'
 
-module.exports = {
+module.exports = (env, { mode }) => {
+  return {
+    entry: "./src/index.js",
+    devtool: mode === 'production' ? false : 'inline-source-map',
 
-  entry: "./src/index.js",
-  devtool: 'inline-source-map',
+    resolve: {
+      alias: {
+        "react": "preact/compat",
+        "react-dom": "preact/compat"
+      }
+    },
 
-  resolve: {
-    alias: {
-      "react": "preact/compat",
-      "react-dom": "preact/compat"
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        },
+      ]
+    },
+
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html"
+      })
+    ],
+
+    output: {
+      path: path.join(__dirname, "/build"),
+      filename: "bundle.js"
+    },
+
+    devServer: {
+      disableHostCheck: true,
+      historyApiFallback: true,
+      clientLogLevel: 'silent',
+      hot: true,
+      open: true,
+      host: myIP,
+      port: port
     }
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-    ]
-  },
-
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html"
-    })
-  ],
-
-  output: {
-    path: path.join(__dirname, "/build"),
-    filename: "bundle.js"
-  },
-
-  devServer: {
-    disableHostCheck: true,
-    historyApiFallback: true,
-    clientLogLevel: 'silent',
-    hot: true,
-    open: true,
-    host: myIP,
-    port: port
   }
 }
