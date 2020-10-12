e01d9beebdf246fedb79d7cc0e84ed967a35b1a1
Ecospell <foretoo@yandex.ru>
2020-10-12 18:28:01 +0300

    change select DOM element to ul. full context integration

--- colire/src/components/select.js
+++ colire/src/components/select.js
@@ -1,28 +1,32 @@
 import { h } from 'preact'
-import { useContext, useRef } from 'preact/hooks'
+import { useContext } from 'preact/hooks'
 import { Context } from '../app/context'
 import './css/select.css'
 
 const Select = () => {
 
-  const { dispatch } = useContext(Context)
-  const ref = useRef()
+  const { state, dispatch } = useContext(Context)
 
-  const handleChange = mode => {
+  const handleChangeMode = mode => {
     dispatch({
       type: 'CHANGE_COLOR_MODE',
       payload: mode
     })
   }
+
   return (
-    <select
-      ref={ref}
-      class='select-color-mode'
-      onChange={() => handleChange(ref.current.value)}
-    >
-      <option value='HEX'>hex</option>
-      <option value='RGB'>rgb</option>
-    </select>
+    <ul class='color-mode-list'>
+      <li
+        class={state.mode === 'HEX' ? 'active' : ''}
+        onClick={() => handleChangeMode('HEX')}>
+        HEX
+      </li>
+      <li
+        class={state.mode === 'RGB' ? 'active' : ''}
+        onClick={() => handleChangeMode('RGB')}>
+        RGB
+      </li>
+    </ul>
   )
 }
 
