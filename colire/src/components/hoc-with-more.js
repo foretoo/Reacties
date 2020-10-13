d68d2c6e84d2ba4dca77e8f27b709878674ffcb8
Borovikov Sergey <serg.there@gmail.com>
2020-10-13 16:20:05 +0300

    ~

--- colire/src/components/hoc-with-more.js
+++ colire/src/components/hoc-with-more.js
@@ -2,8 +2,8 @@ import { h } from 'preact'
 import { useHistory } from "react-router-dom"
 
 const withMore = List => props => {
+  const history = useHistory()
   const WrappedList = List(props).map(Component => {
-    const history = useHistory()
     const handleColorPage = e => {
       e.stopPropagation()
       history.push(Component.props.id)
