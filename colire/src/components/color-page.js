544034a8f9bac100c2b53f2d07d215849d64ea2c
Borovikov Sergey <serg.there@gmail.com>
2020-10-13 16:21:05 +0300

    add: go-back button

--- colire/src/components/color-page.js
+++ colire/src/components/color-page.js
@@ -1,6 +1,6 @@
-import { h } from 'preact'
+import { h, Fragment } from 'preact'
 import { useContext } from 'preact/hooks'
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import { Context } from '../app/context'
 import ColorBox from './color-box'
 import './css/color-page.css'
@@ -10,6 +10,7 @@ const ColorPage = ({ colors }) => {
   if (!colors) {
     const { state } = useContext(Context)
     const { paletteID, colorID } = useParams()
+    const history = useHistory()
     const palette = state.palettes.find(palette => palette.id === paletteID)
     colors = palette[colorID]
   }
@@ -18,7 +19,19 @@ const ColorPage = ({ colors }) => {
   for (const level in colors) {
     colorsList.push(<ColorBox key={level} id={level} addClass=' color' {...colors[level]} />)
   }
-  return colorsList
+
+  const handleGoBack = () => {
+    history.back()
+  }
+
+  return (
+    <>
+      {colorsList}
+      <div class='color-box-back'>
+        <button class='color-box-button' onClick={handleGoBack}>GO BACK</button>
+      </div>
+    </>
+  )
 }
 
 export default ColorPage
