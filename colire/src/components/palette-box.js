9cffde84771a8487595e3a5bd425ea9e8106911e
Ecospell <foretoo@yandex.ru>
2020-10-25 23:43:17 +0300

    add: slash to Link

--- colire/src/components/palette-box.js
+++ colire/src/components/palette-box.js
@@ -9,7 +9,7 @@ const PaletteBox = ({ id, paletteName, colors }) => {
     colorList.push(<div class='palette-box-color' style={style}></div>)
   }
   return (
-    <Link to={`${id}/`} className='palette-box'>
+    <Link to={`/${id}/`} className='palette-box'>
       <div class='palette-box-name'>{paletteName}</div>
       {colorList}
     </Link>
