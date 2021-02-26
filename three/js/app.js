1b696b7a4efc2d9a9d4253b0314758999ec2cca9
Borovikov Sergey <serg.there@gmail.com>
2021-02-26 03:45:01 +0300

    back to THREE

--- three/js/app.js
+++ three/js/app.js
@@ -1,11 +1,4 @@
-import {  Scene,
-          PerspectiveCamera,
-          WebGLRenderer,
-          BoxGeometry,
-          MeshNormalMaterial,
-          ShaderMaterial,
-          Mesh
-        } from 'three'
+import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
@@ -14,15 +7,15 @@ export default class Sketch {
   constructor(options) {
     this.time = 0
     this.container = options.dom
-    this.scene = new Scene();
+    this.scene = new THREE.Scene();
 
     this.width = this.container.offsetWidth
     this.height = this.container.offsetHeight
 
-    this.camera = new PerspectiveCamera( 75, this.width / this.height, 0.1, 1000 );
+    this.camera = new THREE.PerspectiveCamera( 10, this.width / this.height, 0.01, 10 );
     this.camera.position.z = 5;
 
-    this.renderer = new WebGLRenderer({ antialias: true });
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.container.appendChild( this.renderer.domElement );
     this.controls = new OrbitControls( this.camera, this.renderer.domElement )
     this.controls.enablePan = false;
@@ -46,22 +39,24 @@ export default class Sketch {
   }
 
   addObjects() {
-    this.geometry = new BoxGeometry();
-    // this.material = new MeshNormalMaterial();
+    this.geometry = new THREE.PlaneBufferGeometry( .5,.5, 50,50 );
+    // this.material = new THREE.MeshNormalMaterial();
 
-    this.material = new ShaderMaterial({
+    this.material = new THREE.ShaderMaterial({
+      side: THREE.DoubleSide,
       fragmentShader: fragment,
-      vertexShader: vertex
+      vertexShader: vertex,
+      wireframe: true
     })
 
-    this.cube = new Mesh( this.geometry, this.material );
+    this.cube = new THREE.Mesh( this.geometry, this.material );
     this.scene.add( this.cube );
   }
 
   render() {
     this.time += 0.5
-    this.cube.rotation.x += 0.01;
-  	this.cube.rotation.y += 0.01;
+    // this.cube.rotation.x += 0.01;
+  	// this.cube.rotation.y += 0.01;
     this.renderer.render( this.scene, this.camera );
     window.requestAnimationFrame(this.render.bind(this))
   }
