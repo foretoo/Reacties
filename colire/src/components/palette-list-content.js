873a7a9a716895cce0919d92f80469cf1f80e848
Sergey Borovikov <serg.there@gmail.com>
2021-12-03 07:29:41 +0300

    lol: colors is object

--- colire/src/components/palette-list-content.js
+++ colire/src/components/palette-list-content.js
@@ -4,9 +4,10 @@ import './css/palette-content.css'
 
 const PaletteListContent = ({ colors, activeLevel }) => {
 
-  const colorsList = colors.map(color => {
-    return <ColorBox key={color} id={color} addClass=' palette' {...colors[color][activeLevel]} />
-  })
+  const colorsList = []
+  for (const color in colors) {
+    colorsList.push(<ColorBox key={color} id={color} addClass=' palette' {...colors[color][activeLevel]} />)
+  }
   return colorsList
 }
 
