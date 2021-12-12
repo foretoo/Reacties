9086022784aa78140c9cc68dfb5a3b9739ff56ca
Sergey Borovikov <serg.there@gmail.com>
2021-12-12 08:39:45 +0300

    quite styled [2]

--- colire/src/pages/home.js
+++ colire/src/pages/home.js
@@ -1,8 +1,8 @@
 import { h, Fragment } from 'preact'
-import { useContext } from 'preact/hooks'
+import { useContext, useEffect, useRef } from 'preact/hooks'
 import { Link } from 'react-router-dom'
 import { Context } from '@app'
-import { AddPaletteBtn, SVGFilter, PaletteBox } from '@components'
+import { AddPaletteBtn, Footer, SVGFilter, PaletteBox } from '@components'
 import './css/home.css'
 
 const Home = () => {
@@ -10,14 +10,30 @@ const Home = () => {
   const { state: { palettes } } = useContext(Context)
   const paletteLinks = palettes.map( palette => <PaletteBox {...palette}/> )
 
+  const mainRef = useRef()
+  const headRef = useRef()
+  useEffect(() => {
+    const rect = mainRef.current.getBoundingClientRect()
+    headRef.current.style.marginLeft = rect.left + 110 + 'px'
+  }, [])
+
   return (
     <>
-      <main class='home-main'>
+      <header ref={headRef} className='home-header'>
+        <h1>Colllie</h1>
+        <span>yor palette lib</span>
+      </header>
+
+      <main ref={mainRef} className='home-main'>
         <Link to='/create-palette'>
           <AddPaletteBtn />
         </Link>
         {paletteLinks}
       </main>
+
+      <Footer>
+        <a href='https://github.com/foretoo'>github.com/foretoo</a>
+      </Footer>
 
       <SVGFilter />
     </>
