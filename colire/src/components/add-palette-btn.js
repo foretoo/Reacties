11d7db45ee98589cc2493227529dbd66c966649f
Sergey Borovikov <serg.there@gmail.com>
2021-12-14 11:56:34 +0300

    animate add-palette-btn

--- colire/src/components/add-palette-btn.js
+++ colire/src/components/add-palette-btn.js
@@ -4,6 +4,46 @@ import './css/add-palette-btn.css'
 const AddPaletteBtn = () => {
   return (
     <div className='add-palette-btn-container'>
+      <svg className="add-palette-btn-bg" width="250" height="160" viewBox="0 0 250 160" xmlns="http://www.w3.org/2000/svg">
+        <symbol
+          id="rect"
+          width="50"
+          height="40"
+          viewbox="0 0 50 40"
+          fill="white" >
+          <rect
+            x="0"
+            y="0"
+            width="50"
+            height="40"
+            rx="5" />
+        </symbol>
+
+        <use href="#rect" />
+        <use href="#rect" x="50" />
+        <use href="#rect" x="100" />
+        <use href="#rect" x="150" />
+        <use href="#rect" x="200" />
+
+        <use href="#rect" y="40" />
+        <use href="#rect" x="50" y="40" />
+        <use href="#rect" x="100" y="40" />
+        <use href="#rect" x="150" y="40" />
+        <use href="#rect" x="200" y="40" />
+
+        <use href="#rect" y="80" />
+        <use href="#rect" x="50" y="80" />
+        <use href="#rect" x="100" y="80" />
+        <use href="#rect" x="150" y="80" />
+        <use href="#rect" x="200" y="80" />
+
+        <use href="#rect" y="120" />
+        <use href="#rect" x="50" y="120" />
+        <use href="#rect" x="100" y="120" />
+        <use href="#rect" x="150" y="120" />
+        <use href="#rect" x="200" y="120" />
+      </svg>
+
       <input type='button' className='add-palette-btn' value='CREATE PALETTE'></input>
     </div>
   )
