19b0e7a09c511704955871b6fd3219f8ae0e9eb2
Sergey Borovikov <serg.there@gmail.com>
2021-12-14 11:55:56 +0300

    add fancy box-del-icon

--- colire/src/components/palette-box.js
+++ colire/src/components/palette-box.js
@@ -11,6 +11,29 @@ const PaletteBox = ({ id, paletteName, colors }) => {
   return (
     <Link to={`/${id}/`} className='palette-box'>
       <div className='palette-box-name'>{paletteName}</div>
+      <svg className="box-delete-icon"
+        width="40"
+        height="40"
+        viewBox="0 0 40 40"
+        xmlns="http://www.w3.org/2000/svg">
+        <symbol id="line"
+          width="40"
+          height="40"
+          viewBox="0 0 40 40"
+          stroke="#b2bec3"
+          stroke-width="3"
+          stroke-linecap="round" >
+          <line
+            x1="8"
+            y1="20"
+            x2="32"
+            y2="20" />
+        </symbol>
+        <g className="g" >
+          <use className="l1" href="#line" />
+          <use className="l2" href="#line" />
+        </g>
+      </svg>
       <div className='palette-box-content'>
         {colorList}
       </div>
