e2b0c4a6c609f3bb97e2267f4909e0d03d4c588b
Sergey Borovikov <serg.there@gmail.com>
2022-01-14 05:45:07 +0300

    formatted

--- colire/src/utils/helpers/color-scaler.js
+++ colire/src/utils/helpers/color-scaler.js
@@ -2,17 +2,17 @@ import chroma from 'chroma-js'
 
 const levels = [ 100, 200, 300, 400, 500, 600, 700, 800, 900 ]
 
-const getRange = hexColor => {
-  return [
-    chroma(hexColor).darken(1.618).hex(),
-    hexColor,
-    chroma(hexColor).brighten(1.618).hex()
-  ]
-}
+const getRange = hexColor => [
+  chroma(hexColor).darken(1.618).hex(),
+  hexColor,
+  chroma(hexColor).brighten(1.618).hex()
+]
 
-const getScale = (hexColor, numOfColors) => {
-  return chroma.scale(getRange(hexColor)).mode('lab').colors(numOfColors)
-}
+const getScale = (hexColor, numOfColors) => (
+  chroma.scale(getRange(hexColor))
+    .mode('lab')
+    .colors(numOfColors)
+)
 
 const colorScaler = palette => {
   const newPalette = {
