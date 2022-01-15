9d2fd179096b88c3e39f4e6a1a7fe66d1e15e9ad
Sergey Borovikov <serg.there@gmail.com>
2022-01-16 01:24:10 +0300

    improve readability

--- colire/src/components/slider-neu.js
+++ colire/src/components/slider-neu.js
@@ -11,60 +11,75 @@ const SliderNeu = ({
   onChange = () => {},
 }) => {
 
-  const initialState = {
-    start: false,
-    pointerX: 0,
-    offset: 0,
-    translate: 0,
-    translateStep: 0,
-    width: 0,
-    x: 0,
+  const INIT = {
+    pointer: {
+      start: false,
+      x: 0,
+    },
+    path: {
+      width: 0,
+      x: 0,
+    },
+    handler: {
+      offset: 0,
+      translate: 0,
+      translateStep: 0,
+    },
     value: 0,
   }
 
-  const [ state, setState ] = useState(initialState)
-  const pathRef = useRef(null)
-  const handlerRef = useRef(null)
+  const [ GET, SET ] = useState(INIT)
+  const pathRef = useRef()
+  const handlerRef = useRef()
 
   useEffect(() => {
 
-    const pathRect = pathRef.current.getBoundingClientRect()
-    const width = pathRect.width - pathRect.height
-    const x = pathRect.x
-    const translateStep = width / ((max - min) / step)
+    const { width: pathWidth, x: pathX } = pathRef.current.getBoundingClientRect()
+    const { width: handlerWidth } = handlerRef.current.getBoundingClientRect()
+    const path = { width: pathWidth - handlerWidth, x: pathX }
+
+    const translateStep = path.width / ((max - min) / step)
     const offset = ((defaultValue - min) / step) * translateStep
     const translate = offset
-    setState({ ...state, offset, translate, translateStep, width, x })
+    const handler = { offset, translate, translateStep }
 
-    window.addEventListener("pointerup", handleEnd, false);
-    window.addEventListener("pointercancel", handleEnd, false);
+    SET({ ...GET, path, handler })
+
+    window.addEventListener("pointerup", handleEnd, false)
+    window.addEventListener("pointercancel", handleEnd, false)
 
     return () => {
-      window.removeEventListener("pointerup", handleEnd);
-      window.removeEventListener("pointercancel", handleEnd);
+      window.removeEventListener("pointerup", handleEnd)
+      window.removeEventListener("pointercancel", handleEnd)
     }
   }, [])
 
   const handleStart = (e) => {
     handlerRef.current.setPointerCapture(e.pointerId)
-    setState(prevState => ({ ...prevState, start: true, pointerX: e.pageX }))
+    SET(PREV => ({ ...PREV, pointer: { start: true, x: e.pageX } }))
   }
   const handleEnd = (e) => {
-    handlerRef.current.releasePointerCapture(e.pointerId);
-    setState(prevState => ({ ...prevState, start: false }))
+    handlerRef.current.releasePointerCapture(e.pointerId)
+    SET(PREV => ({ ...PREV, pointer: { ...PREV.pointer, start: false } }))
   }
+  
   const handleMove = (e) => {
-    state.start &&
-    e.pageX >= state.x &&
-    e.pageX <= state.x + state.width &&
-    setState(prevState => {
-      const pointerX = e.pageX
-      const offset = clamp(prevState.offset + (e.pageX - prevState.pointerX), 0, state.width)
-      const steps = Math.round((offset / state.width) / (step / (max - min)))
-      const translate = steps * state.translateStep
+    GET.pointer.start &&
+    e.pageX >= GET.path.x &&
+    e.pageX <= GET.path.x + GET.path.width &&
+    SET(PREV => {
+
+      const pointer = { ...PREV.pointer, x: e.pageX }
+      const delta = pointer.x - PREV.pointer.x
+      const offset = clamp(PREV.handler.offset + delta, 0, GET.path.width)
+      const steps = Math.round((offset / GET.path.width) / (step / (max - min)))
+      const translate = steps * GET.handler.translateStep
+      const handler = { ...PREV.handler, offset, translate }
+
       const value = min + steps * step
-      if (prevState.value !== value) onChange(value)
-      return { ...prevState, pointerX, offset, translate, value }
+      if (PREV.value !== value) onChange(value)
+
+      return { ...PREV, pointer, handler, value }
     })
   }
 
@@ -72,7 +87,7 @@ const SliderNeu = ({
     <div className='slider-container'>
       <div ref={pathRef} className='slider-path'>
         <div ref={handlerRef} className='slider-handler'
-          style={{ transform: `translate(${state.translate}px)` }}
+          style={{ transform: `translate(${GET.handler.translate}px)` }}
           onPointerDown={handleStart}
           onPointerMove={handleMove} >
         </div>
