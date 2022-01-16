10f5c4d858b503253a80a0c17aa4896e03058506
Sergey Borovikov <serg.there@gmail.com>
2022-01-17 02:44:37 +0300

    ~

--- colire/src/index.js
+++ colire/src/index.js
@@ -1,7 +1,6 @@
 import { h, render } from 'preact'
 import { HashRouter as Router } from 'react-router-dom'
 import { ContextProvider, App } from '@app'
-import { SliderNeu } from '@components'
 import './index.css'
 
 const root = document.createElement('div')
