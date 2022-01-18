a017624629d43db0e77f27ab0c11b7b4de353d9d
Sergey Borovikov <serg.there@gmail.com>
2022-01-18 08:56:10 +0300

    upd: switcher can handle multiple options

--- colire/src/components/switcher.js
+++ colire/src/components/switcher.js
@@ -3,41 +3,43 @@ import { useState, useEffect, useRef } from 'preact/hooks'
 import './css/switcher.css'
 
 const Switcher = ({
-  option1 = "on",
-  option2 = "off",
-  defaultValue = "off",
+  options = [ "on", "off" ],
+  defaultValue = options[0],
   onChange = () => {},
   style = { "--font-size": "16px", "--padding": "10px", "--border": "2px" }
 }) => {
 
-  catchError(option1, option2, defaultValue)
+  checkOptions(options, defaultValue)
 
   const INIT = {
-    handler: {
-      option1Width: 0,
-      option2Width: 0,
-      translate: 0,
-      mount: false,
-    },
+    options: [
+      {
+        value: "",
+        width: 0,
+        translate: 0,
+      }
+    ],
+    mounted: false,
     value: defaultValue,
   }
 
   const [ GET, SET ] = useState(INIT)
-  const option1Ref = useRef()
-  const option2Ref = useRef()
+  const optionsRef = useRef([])
 
   useEffect(() => {
-    const option1Rect = option1Ref.current.getBoundingClientRect()
-    const option2Rect = option2Ref.current.getBoundingClientRect()
+    optionsRef.current = optionsRef.current.slice(0, options.length)
 
-    const option1Width = option1Rect.width
-    const option2Width = option2Rect.width
-    const translate = option2Rect.x - option1Rect.x
-    const mount = option1Width && option2Width && translate
-    const handler = { option1Width, option2Width, translate, mount }
+    let translate = 0
+    const optionsData = optionsRef.current.reduce((acc, div, i) => {
+      const width = div.getBoundingClientRect().width
+      translate = i ? translate + acc[i - 1].width : 0
+      return [ ...acc, { value: options[i], width, translate }]
+    }, [])
 
-    SET(PREV => ({ ...PREV, handler }))
-  }, [])
+    const mounted = optionsData.every(({ width }) => width > 0 )
+
+    SET(PREV => ({ ...PREV, options: optionsData, mounted }))
+  }, [ options ])
 
   const handleSelect = (option) => {
     if (option !== GET.value) {
@@ -46,50 +48,51 @@ const Switcher = ({
     }
   }
 
-  const handlerAnimation = {
-    width: `${GET.value === option1 ? GET.handler.option1Width : GET.handler.option2Width}px`,
-    transform: `translate(${GET.value === option2 ? GET.handler.translate : 0}px)`,
+  const getAnimation = () => {
+    const { width, translate } = GET.options.find(({ value }) => value === GET.value)
+    return {
+      width: `${width}px`,
+      transform: `translate(${translate}px)`,
+    }
   }
 
   return (
     <div className='switcher-container' style={style}>
       <div className='switcher'>
-        <div
-          ref={option1Ref}
-          className={`switch-case${GET.value === option1 ? " active" : ""}`}
-          onClick={() => handleSelect(option1)} >
-          {option1}
-        </div>
-        <div
-          ref={option2Ref}
-          className={`switch-case${GET.value === option2 ? " active" : ""}`}
-          onClick={() => handleSelect(option2)} >
-          {option2}
-        </div>
-        {GET.handler.mount && (
-          <div className='switch-case switch-handler' style={handlerAnimation}></div>
+        {options.map((option, i) => (
+          <div
+            ref={div => optionsRef.current[i] = div}
+            className={`switch-case${GET.value === option ? " active" : ""}`}
+            onClick={() => handleSelect(option)} >
+            {option}
+          </div>
+        ))}
+        {GET.mounted && (
+          <div className='switch-case switch-handler' style={getAnimation()}></div>
         )}
       </div>
     </div>
   )
 }
 
-const catchError = (option1, option2, defaultValue) => {
+const checkOptions = (options, defaultValue) => {
+  if (options.length < 2) {
+    throw Error("options number must be 2 at least")
+  }
   if (
-    typeof option1 !== "string" ||
-    typeof option2 !== "string" ||
+    options.some(option => typeof option !== "string") ||
     typeof defaultValue !== "string"
   ) {
     throw Error("options and defaultValue must be typeof string")
   }
-  if (option1.trim() === "" || option2.trim() === "") {
+  if (options.some(option => option.trim() === "")) {
     throw Error("options must not be an empty string")
   }
-  if (option1 === option2) {
-    throw Error("options must not be equal to providing onChange functionality")
+  if (options.length !== new Set(options).size) {
+    throw Error("options must not equal each other for providing onChange functionality")
   }
-  if (defaultValue !== option1 && defaultValue !== option2) {
-    throw Error("defaultValue must equals either option1 or option2")
+  if (!options.includes(defaultValue)) {
+    throw Error("defaultValue must equals one of the options")
   }
 }
 
