0587df60f98e87a9aa1806816513251ed13b1c78
Sergey Borovikov <serg.there@gmail.com>
2022-01-20 09:13:15 +0300

    picker rotating!

--- colire/src/components/color-picker.js
+++ colire/src/components/color-picker.js
@@ -1,5 +1,6 @@
 import { h } from 'preact'
 import { useState, useEffect, useRef } from 'preact/hooks'
+import { calcAngle } from '@utils/helpers'
 import './css/color-picker.css'
 
 const ColorPicker = () => {
@@ -7,15 +8,24 @@ const ColorPicker = () => {
   const initialPicker = {
     pointer: {
       start: false,
-      x: 0,
-      y: 0,
+      a: 0,
     },
-    angle: 0,
+    picker: {
+      origin: { x: 0, y: 0 },
+      a: 0,
+    },
+    value: [ 255, 0, 0 ],
   }
   const [ GET, SET ] = useState(initialPicker)
   const pickerRef = useRef()
 
   useEffect(() => {
+    const pickerRect = pickerRef.current.getBoundingClientRect()
+    const x = pickerRect.x + pickerRect.width / 2
+    const y = pickerRect.y + pickerRect.height / 2
+    const origin = { x, y }
+    SET(PREV => ({ ...PREV, picker: { ...PREV.picker, origin } }))
+
     window.addEventListener("pointerup", handleEnd, false)
     window.addEventListener("pointercancel", handleEnd, false)
     return () => {
@@ -26,22 +36,41 @@ const ColorPicker = () => {
 
   const handleStart = (e) => {
     pickerRef.current.setPointerCapture(e.pointerId)
-    SET(PREV => ({ ...PREV, pointer: { ...PREV.pointer, start: true } }))
+    const a = calcAngle(
+      e.pageX - GET.picker.origin.x,
+      e.pageY - GET.picker.origin.y
+    )
+    SET(PREV => ({ ...PREV, pointer: { start: true, a } }))
   }
   const handleEnd = (e) => {
     pickerRef.current.releasePointerCapture(e.pointerId)
     SET(PREV => ({ ...PREV, pointer: { ...PREV.pointer, start: false } }))
   }
   const handleMove = (e) => {
-    GET.pointer.start && 
-    console.log(`MOVE`)
+    GET.pointer.start &&
+    SET(PREV => {
+      const pointer = {
+        ...PREV.pointer,
+        a: calcAngle(
+          e.pageX - GET.picker.origin.x,
+          e.pageY - GET.picker.origin.y
+        )
+      }
+      const delta = pointer.a - PREV.pointer.a
+      const picker = {
+        ...PREV.picker,
+        a: (PREV.picker.a + delta) % 360
+      }
+      if (picker.a < 0) picker.a += 360
+      return { ...PREV, pointer, picker }
+    })
   }
 
   return (
     <div className='color-picker-container'>
       <div className='picker-colors'>
         <div ref={pickerRef} className='picker'
-          style={{ transform: `rotate${GET.angle}deg` }}
+          style={{ transform: `rotate(${GET.picker.a}deg)` }}
           onPointerDown={handleStart}
           onPointerMove={handleMove} >
           <svg className='picker-fill'
@@ -49,8 +78,8 @@ const ColorPicker = () => {
             height="228"
             viewBox="0 0 228 228"
             xmlns="http://www.w3.org/2000/svg" >
-            <path d="M154 100 h69 c2 0 4 -2 4 -4 a114 114 0 1 0 0 37 c0 -3 -2 -5 -4 -5 h-69 a14 14 0 0 1 0 -28Z"/>
-            <path d="M154 122 a8 8 0 0 1 0 -16 h70 a4 4 0 0 1 4 4 v8 a4 4 0 0 1 -4 4Z" fill="#fff" />
+            <path d="M154 100a14 14 0 0 0 0 28h68.63a4 4 0 0 1 3.88 4.48a114 114 0 1 1 0-36.96a4 4 0 0 1-3.88 4.48Z" />
+            <path d="M154 122 a8 8 0 0 1 0-16 h70 a4 4 0 0 1 4 4 v8 a4 4 0 0 1-4 4Z" fill="#fff" />
             <circle cx='114' cy='114' r='14' fill="#222" />
           </svg>
         </div>
@@ -60,4 +89,3 @@ const ColorPicker = () => {
 }
 
 export default ColorPicker
-//
