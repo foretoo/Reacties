98cec28517faa10e152dcdaeee96f9de4692c2d0
Sergey Borovikov <serg.there@gmail.com>
2022-01-20 21:06:19 +0300

    add colorToAngle function

--- colire/src/utils/helpers/index.js
+++ colire/src/utils/helpers/index.js
@@ -1,12 +1,14 @@
 import addLevelProp from './add-level-prop'
 import angleToColor from './angle-to-color'
 import colorScaler from './color-scaler'
+import colorToAngle from './color-to-angle'
 import { calcAngle, clamp, round } from './math'
 
 export {
   addLevelProp,
   angleToColor,
   colorScaler,
+  colorToAngle,
   calcAngle,
   clamp,
   round,
