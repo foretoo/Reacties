f001bade747901ee1da74e2711ce339f57e6c2f2
Sergey Borovikov <serg.there@gmail.com>
2022-01-23 13:21:00 +0300

    refactor labels, handler-svg, css

--- colire/src/components/color-picker.js
+++ colire/src/components/color-picker.js
@@ -1,13 +1,14 @@
 import { h } from 'preact'
 import { useState, useEffect, useRef } from 'preact/hooks'
-import { calcAngle } from '@utils/helpers'
+import { calcAngle, r3d } from '@utils/helpers'
 import chroma from 'chroma-js'
 import './css/color-picker.css'
 
 const ColorPicker = ({
   mode = 'hsl',
   defaultValue = { h:270, s:100, l:50 },
-  shift = 90,
+  shift = 0,
+  size = 320,
 }) => {
 
 
@@ -65,7 +66,7 @@ const ColorPicker = ({
       const a = calcAngle(
         e.pageX - GET.handler.origin.x,
         e.pageY - GET.handler.origin.y
-      ) - GET.shift
+      ) - 90 - GET.shift
       let h = PREV.h
       if (e.target.className === "color-picker") {
         pickerRef.current.setPointerCapture(e.pointerId)
@@ -104,7 +105,7 @@ const ColorPicker = ({
           a: calcAngle(
             e.pageX - GET.handler.origin.x,
             e.pageY - GET.handler.origin.y
-          ) - GET.shift
+          ) - 90 - GET.shift
         }
         let a = (PREV.handler.a + (pointer.a - PREV.pointer.a)) % 360
         if (a < 0) a += 360
@@ -125,11 +126,11 @@ const ColorPicker = ({
 
 
   return (
-    <div className='color-picker-container'>
+    <div className='color-picker-container' style={{ "--size": `${size}px` }}>
       
       <div ref={pickerRef} className='color-picker'
         style={{ background:`conic-gradient(
-          from ${0.25 + GET.shift / 360}turn,
+          from ${0.5 + GET.shift / 360}turn,
             hsl(0,   ${GET.s}%, ${GET.l}%),
             hsl(60,  ${GET.s}%, ${GET.l}%),
             hsl(120, ${GET.s}%, ${GET.l}%),
@@ -143,32 +144,52 @@ const ColorPicker = ({
 
         <div className='color-picker-labels' style={{ transform: `rotate(${GET.shift}deg)` }}>
           <label style={{ "--sign":
-              (GET.shift <  180 && GET.shift >= 0) ||
-              (GET.shift < -180)
-                ? -1 : 1
+              (GET.shift >=  -90 && GET.shift <   90) ||
+              (GET.shift >=  270 || GET.shift < -270)
+                ? 0 : 1
             }}>R</label>
           <label style={{ "--sign":
-              (GET.shift <  60  && GET.shift >= -120) ||
-              (GET.shift < -300 || GET.shift >=  240)
-                ? 1 : -5
+              (GET.shift >=  -30 && GET.shift <  150) ||
+              (GET.shift >=  330 || GET.shift < -210)
+                ? 1 : -2
             }}>G</label>
           <label style={{ "--sign":
-              (GET.shift <  300 && GET.shift >=  120) ||
-              (GET.shift < -60  && GET.shift >= -240)
-                ? 5 : -1
+              (GET.shift >= -150 && GET.shift <   30) ||
+              (GET.shift >=  210 || GET.shift < -330)
+                ? 1 : -2
             }}>B</label>
         </div>
 
         <div ref={handlerRef} className='picker-handler'>
           { GET.mounted &&
-          <svg className='picker-handler-view' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 228 228"
-            style={{ transform: `rotate(calc(${GET.handler.a + GET.shift}deg))` }} >
-            <path d="M154 100a14 14 0 0 0 0 28h68.63a4 4 0 0 1 3.88 4.48a114 114 0 1 1 0-36.96a4 4 0 0 1-3.88 4.48Z"
-              fill="#333" />
-            <path d="M154 122 a8 8 0 0 1 0-16 h70 a4 4 0 0 1 4 4 v8 a4 4 0 0 1-4 4Z"
+          <svg className='picker-handler-view'
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 60 60"
+            style={{ transform: `rotate(${GET.handler.a + GET.shift}deg)` }} >
+            <circle cx="30" cy="30" r="30" fill="#2a2a2a" />
+            <g transform='rotate(90,30,30)'>
+              <radialGradient id="grad">
+                <stop stop-color="#2a2a2a" offset="80%" />
+                <stop stop-color="#0a0a0a" offset="100%" />
+              </radialGradient>
+              <path d={Array(120).fill("").reduce((path,_,i,a) => {
+                const [ r, deg ] = [ (Math.PI / a.length * 2) * i, (360 / a.length) * i ]
+                if (deg < 12 || deg > 348) return path
+                const [ w, h ] = [ 6, 0.7 ]
+                const [ x, y ] = [
+                  r3d(30 + (28 - w) * Math.cos(r) + (h / 2) * Math.sin(r)),
+                  r3d(30 + (28 - w) * Math.sin(r) - (h / 2) * Math.cos(r))
+                ]
+                return `${path} M${x} ${y}
+                  l${r3d( w * Math.cos(r))} ${r3d( w * Math.sin(r))}
+                  l${r3d(-h * Math.sin(r))} ${r3d( h * Math.cos(r))}
+                  l${r3d(-w * Math.cos(r))} ${r3d(-w * Math.sin(r))}
+                  Z`.replace(/\s+(?=[MlZ])/g, "")
+                }, "")}
+                fill="url(#grad)" />
+            </g>
+            <path d="M27 43a3 3 0 0 1 6 0v14a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1Z"
               fill={`hsl(${GET.h}, ${GET.s}%, ${GET.l}%)`} />
-            <circle cx='114' cy='114' r='14'
-              fill="#222" />
           </svg> }
         </div>
 
