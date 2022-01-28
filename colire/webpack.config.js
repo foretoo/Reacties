f46dea0f15e238c4bb71cee6cf9d2ba8934ebde0
Sergey Borovikov <serg.there@gmail.com>
2022-01-28 12:04:15 +0300

    ~

--- colire/webpack.config.js
+++ colire/webpack.config.js
@@ -1,75 +1,75 @@
-const path = require('path')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const homeIP = '192.168.1.44'
-const katIP = '192.168.0.103'
-const port = '8000'
+const path = require("path")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const homeIP = "192.168.1.44"
+const katIP = "192.168.0.103"
+const local = "localhost"
 
 module.exports = (env, { mode }) => ({
   entry: {
-    index: './src/index.js'
+    index: "./src/index.js",
   },
-  devtool: mode === 'development' ? 'inline-source-map' : false,
+  devtool: mode === "development" ? "inline-source-map" : false,
 
   resolve: {
     alias: {
-      'react': 'preact/compat',
-      'react-dom': 'preact/compat',
-      '@app': path.resolve(__dirname, 'src/app'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-    }
+      "react":       "preact/compat",
+      "react-dom":   "preact/compat",
+      "@app":        path.resolve(__dirname, "src/app"),
+      "@assets":     path.resolve(__dirname, "src/assets"),
+      "@pages":      path.resolve(__dirname, "src/pages"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@utils":      path.resolve(__dirname, "src/utils"),
+    },
   },
 
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test:    /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader:  "babel-loader",
       },
       {
         test: /\.css$/,
-        use: [
+        use:  [
           MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
+          "css-loader",
+        ],
       },
       {
-        test: /\.(png|jpg|gif|svg)$/i,
-        loader: 'url-loader'
-      }
-    ]
+        test:   /\.(png|jpg|gif|svg)$/i,
+        loader: "url-loader",
+      },
+    ],
   },
 
   plugins: [
     new HtmlWebPackPlugin({
-      title: 'Colire',
-      meta: { viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
-      minify: false
+      title:  "Colire",
+      meta:   { viewport: "width=device-width, initial-scale=1, shrink-to-fit=no" },
+      minify: false,
     }),
     new MiniCssExtractPlugin({
-      filename: 'dist/[name]' + (mode === 'development' ? '' : '.[contenthash]') + '.css'
-    })
+      filename: "dist/[name]" + (mode === "development" ? "" : ".[contenthash:8]") + ".css",
+    }),
   ],
 
   output: {
-    path: path.join(__dirname, ''),
-    filename: 'dist/[name].[contenthash].js',
-    chunkFilename: ({ chunk }) => 'dist/' + (chunk.name || chunk.id) + '.vendor.js',
-    hashDigestLength: 16,
+    path:             path.join(__dirname, ""),
+    filename:         "dist/[name].[contenthash:8].js",
+    chunkFilename:    ({ chunk }) => "dist/" + (chunk.name || chunk.id) + ".vendor.js",
   },
 
   devServer: {
     historyApiFallback: true,
-    client: {
-      logging: 'none',
+    client:             {
+      logging: "none",
     },
-    allowedHosts: 'auto',
-    hot: true,
-    open: true,
-    host: homeIP,
-    port: port
-  }
+    allowedHosts: "auto",
+    hot:          true,
+    open:         true,
+    host:         homeIP,
+    port:         8000,
+  },
 })
