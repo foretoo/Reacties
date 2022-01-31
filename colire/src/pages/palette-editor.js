d476cb4b85531c920eee8fbc9984a89f4f290a6b
Sergey Borovikov <serg.there@gmail.com>
2022-01-31 15:08:31 +0300

    linted

--- colire/src/pages/palette-editor.js
+++ colire/src/pages/palette-editor.js
@@ -2,7 +2,12 @@ import { h, Fragment } from "preact"
 import { useContext } from "preact/hooks"
 import { Link } from "react-router-dom"
 import { Context } from "@app"
-import { Header, SortablePalette, NewPaletteForm, NewPaletteNameForm } from "@components"
+import {
+  Header,
+  SortablePalette,
+  NewPaletteForm,
+  NewPaletteNameForm,
+} from "@components"
 import "./css/palette-editor.css"
 
 const PaletteEditor = () => {
@@ -13,9 +18,12 @@ const PaletteEditor = () => {
     <>
       <Header class="new-palette-header">
         <div class="nav">
-          <svg class="burger-icon" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"
-            onClick={() => dispatch({ type: "TOGGLE_NEW_COLOR_FORM" })}
-          >
+          <svg class="burger-icon"
+            width="20" height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            onClick={() => dispatch({ type: "TOGGLE_NEW_COLOR_FORM" })} >
             <path d="M18 10H2M18 4H2M18 16H2" strokeWidth="2" strokeLinecap="round"/>
           </svg>
           <Link to="/" className="page-header-link">Home</Link>
