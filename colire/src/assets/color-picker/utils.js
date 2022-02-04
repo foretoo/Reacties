44161db1aa0f910f5992c19fe0c6e0a8cb021e57
Sergey Borovikov <serg.there@gmail.com>
2022-02-04 04:24:11 +0300

    ~

--- colire/src/assets/color-picker/utils.js
+++ colire/src/assets/color-picker/utils.js
@@ -4,10 +4,7 @@ const clamp = (num, min, max) => num < min ? min : num > max ? max : num
 
 const round_dec = (n, d) => round((n + Number.EPSILON) * (10 * d)) / (10 * d)
 
-const calc_angle = (x, y) => {
-  const angle = Math.atan2(y, x) * (180 / Math.PI)
-  return angle < 0 ? angle + 360 : angle
-}
+const calc_angle = (x, y) => (Math.atan2(y, x) * (180 / Math.PI) + 360) % 360
 
 export {
   round,
