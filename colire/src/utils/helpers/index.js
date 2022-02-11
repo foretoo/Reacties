441a29a5c3127e3a376e766d3441750646edee48
Sergey Borovikov <serg.there@gmail.com>
2022-02-11 04:51:17 +0300

    no need such function

--- colire/src/utils/helpers/index.js
+++ colire/src/utils/helpers/index.js
@@ -1,14 +1,10 @@
 import addLevelProp from "./add-level-prop"
-import angleToColor from "./angle-to-color"
 import colorScaler from "./color-scaler"
-import colorToAngle from "./color-to-angle"
 import { calcAngle, clamp, round, r3d } from "./math"
 
 export {
   addLevelProp,
-  angleToColor,
   colorScaler,
-  colorToAngle,
   calcAngle,
   clamp,
   round,
