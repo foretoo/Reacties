1a8f36cd1154e37d4fa91974ecc3e1f69c2c5380
Sergey Borovikov <serg.there@gmail.com>
2022-02-16 21:38:58 +0300

    theme switcher ready

--- colire/src/app/app.js
+++ colire/src/app/app.js
@@ -1,20 +1,26 @@
 import { h, Fragment } from "preact"
 import { Switch, Route } from "react-router-dom"
+import { useContext } from "preact/hooks"
+import { Context } from "@app"
 import { Home, Palette, PaletteEditor } from "@pages"
 import { Footer, SwitcherTheme } from "@components"
+import "./app.css"
 
-const App = () => (
-  <>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/create-palette" component={PaletteEditor} />
-      <Route path="/:paletteID/:colorID?" component={Palette} />
-    </Switch>
-    <Footer>
-      <SwitcherTheme />
-      <a href="https://github.com/foretoo" target="_blank">github.com/foretoo</a>
-    </Footer>
-  </>
-)
+const App = () => {
+  const { state: { theme }} = useContext(Context)
+  return (
+    <div className={`app` + (theme === `dark` ? ` dark-theme` : ``)}>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/create-palette" component={PaletteEditor}/>
+        <Route path="/:paletteID/:colorID?" component={Palette}/>
+      </Switch>
+      <Footer>
+        <SwitcherTheme/>
+        <a href="https://github.com/foretoo" target="_blank">github.com/foretoo</a>
+      </Footer>
+    </div>
+  )
+}
 
 export default App
