7af951faca6789c831371cab1a2bcc07625cccd3
Sergey Borovikov <serg.there@gmail.com>
2022-02-26 13:56:30 +0300

    fix: animation wiggling on hover

--- colire/src/components/add-palette-btn.js
+++ colire/src/components/add-palette-btn.js
@@ -1,7 +1,11 @@
+import gsap from "gsap"
 import { h } from "preact"
+import { useEffect, useRef } from "preact/hooks"
 import "./css/add-palette-btn.css"
 
 const AddPaletteBtn = () => {
+
+  const ref = useRef(null)
 
   const rects = []
   for (let y = 0; y <= 120; y += 40) {
@@ -10,22 +14,34 @@ const AddPaletteBtn = () => {
     }
   }
 
+  useEffect(() => {
+    gsap.set(ref.current, { scaleX: 0.84, scaleY: 0.8, transformOrigin: "center" })
+    gsap.defaults({ duration: 0.3 })
+  }, [])
+  const handleHover = () => {
+    gsap.to(ref.current, { attr: { rx: 0 }, scale: 1 })
+  }
+  const handleIdle = () => {
+    gsap.to(ref.current, { attr: { rx: 5 },scaleX: 0.84, scaleY: 0.8 })
+  }
+
   return (
     <div className="add-palette-btn-container">
       <input className="add-palette-btn"
         type="button"
-        value="CREATE PALETTE" />
+        value="CREATE PALETTE"
+        onMouseEnter={handleHover}
+        onMouseLeave={handleIdle} />
       <svg className="add-palette-btn-bg"
         width="250"
         height="160"
         viewBox="0 0 250 160"
-        fill="white"
         xmlns="http://www.w3.org/2000/svg" >
 
         <symbol id="rect"
           width="50"
           height="40" >
-          <rect
+          <rect ref={ref}
             x="0"
             y="0"
             width="50"
