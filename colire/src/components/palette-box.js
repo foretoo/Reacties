be823e637da3c75e15acca241c55a465e6d38a7f
Sergey Borovikov <serg.there@gmail.com>
2022-02-27 08:30:14 +0300

    set animation from gsap

--- colire/src/components/palette-box.js
+++ colire/src/components/palette-box.js
@@ -1,19 +1,37 @@
 import { h } from "preact"
+import { useRef } from "preact/hooks"
 import { Link } from "react-router-dom"
+import gsap from "gsap"
 import "./css/palette-box.css"
 
 const PaletteBox = ({ id, name, colors }) => {
+
+  const contentRef = useRef(null)
+  gsap.defaults({ duration: 0.5 })
+
   const colorList = []
   for (const color in colors) {
     const style = { background: colors[color].levels[4].hex }
     colorList.push(<div className="palette-box-color" style={style}></div>)
   }
+
+  const handleMouseEnter = () => {
+    gsap.to(contentRef.current, { scale: 1.05 })
+  }
+  const handleMouseLeave = () => {
+    gsap.to(contentRef.current, { scale: 1 })
+  }
+
   return (
-    <div className="palette-box-container">
+    <div className="palette-box-container"
+      style={{ filter: "none" }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave} >
 
       <Link to={`/${id}/`} className="palette-box">
         <div className="palette-box-name">{name}</div>
-        <div className="palette-box-content">
+        <div className="palette-box-content"
+          ref={contentRef} >
           {colorList}
         </div>
       </Link>
