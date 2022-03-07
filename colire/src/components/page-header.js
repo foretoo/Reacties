068102d6539aecb5a3b41c6b507be30d7c2a6df8
Sergey Borovikov <serg.there@gmail.com>
2022-03-07 14:25:36 +0300

    add header its mobile version

--- colire/src/components/page-header.js
+++ colire/src/components/page-header.js
@@ -1,7 +1,7 @@
 import { h, Fragment } from "preact"
+import { useRef } from "preact/hooks"
 import { Link, useHistory } from "react-router-dom"
-import { useAgent } from "@app/ctx"
-import { usePalettesDispatch } from "@app/ctx"
+import { useAgent, usePalettesDispatch } from "@app/ctx"
 import { useConst } from "@utils/hooks"
 import { Button, Header } from "@assets"
 import "./css/header.css"
@@ -10,7 +10,9 @@ const PageHeader = ({ palette, color, editor }) => {
 
   const history = useHistory()
   const dispatch = usePalettesDispatch()
-  const { agent: { width }} = useAgent()
+  const { agent } = useAgent()
+  const isMobile = agent.width < 769 ? true : false
+  const menuRef = useRef()
 
   const handleDeletePalette = useConst(() => {
     dispatch({
@@ -22,56 +24,110 @@ const PageHeader = ({ palette, color, editor }) => {
   const handleEditPalette = useConst(() => {
     history.push("edit")
   })
+  const handleBack = useConst((path) => {
+    const link = path[path.length - 2].link
+    history.push(link)
+  })
+  const toggleMenu = useConst(() => {
+    // menuRef.current.classList.toggle("hidden")
+    document.querySelector(".header").classList.toggle("expanded")
+  })
 
-  const isCreatePalettePage =  editor && !palette
-  const isPalettePage       = !editor && !color
+  const getPath = () => {
+    const path = [{ name: "root", link: "/" }]
+    if (editor && !palette) {
+      path.push({ name: "Create palette" })
+      return path
+    }
+    else if (!editor && !color) {
+      path.push({
+        name: palette.name,
+        emoji: palette.emoji
+      })
+      return path
+    }
+    else {
+      path.push({
+        name: palette.name,
+        emoji: palette.emoji,
+        link: `/${palette.id}/`
+      })
+      if (editor) {
+        path.push({ name: "edit" })
+        return path
+      }
+      else {
+        path.push({ name: color.name })
+        return path
+      }
+    }
+  }
+  const Navigation = ({ path }) => (
+    path.map(({ link, name, emoji }) => (
+      link
+      ? <>
+          <Link className="nav-link" to={link}>
+            <span>{name}</span>
+            {emoji ? <span className="nav-emoji">{emoji}</span> : null}
+          </Link>
+          <span className="nav-slash">/</span>
+        </>
+      : <>
+          <span className="nav-current">{name}</span>
+          {emoji ? <span className="nav-emoji">{emoji}</span> : null}
+        </>
+    ))
+  )
+  const Back = ({ path }) => (
+    <>
+      <span className="nav-link" onClick={() => handleBack(path)}>back</span>
+      {(editor && palette)
+        ? <>
+            <span className="nav-current">{palette.name}</span>
+            <span className="nav-emoji">{palette.emoji}</span>
+          </>
+        : <Navigation path={path.slice(-1)} />
+      }
+    </>
+  )
+  const Menu = () => (
+    <div ref={menuRef}
+      className={
+        /*`header-menu${isMobile ? ` hidden` : ``}`*/
+        "header-menu"
+        }>
+      <Button name="Export"
+        type="idle"
+        size={33}
+        onClick={() => {}} />
+      {!color &&
+        <>
+          <Button name="Edit"
+            type="idle"
+            size={33}
+            onClick={handleEditPalette} />
+          <Button name="Delete"
+            type="idle"
+            size={33}
+            onClick={handleDeletePalette} />
+        </>
+      }
+    </div>
+  )
+  const MenuIcon = () => (
+    <span className="menu-icon" onClick={toggleMenu}>menu</span>
+  )
 
   return (
-    <Header>
+    <Header className={isMobile ? `mobile` : ``}>
       <nav className="header-nav">
-        <Link className="nav-link" to="/">root</Link>
-        <span className="nav-slash">/</span>
-        {(isCreatePalettePage || isPalettePage)
-          ? <>
-              <span className="nav-palette-name">
-                {editor ? "Create palette": palette.name}
-              </span>
-              <span className="nav-palette-emoji">
-                {editor ? "🧑‍🎨" : palette.emoji}
-              </span>
-            </>
-          : <> {/* IF "PALETTE EDIT" || "COLOR" PAGE */}
-              <Link to={`/${palette.id}/`}>
-                <span>{palette.name}</span>
-                <span className="nav-palette-emoji">{palette.emoji}</span>
-              </Link>
-              <span className="nav-slash">/</span>
-              <span className="nav-palette-name">
-                {editor ? "edit" : color.name}
-              </span>
-            </>
+        {!isMobile
+          ? <Navigation path={getPath()} />
+          : <Back path={getPath()} />
         }
+        {isMobile && !editor && <MenuIcon />}
       </nav>
-      {!editor &&
-        <div className="header-menu">
-          <Button name="Export"
-            type="idle"
-            size={35}
-            onClick={() => {}} />
-          {!color &&
-            <>
-              <Button name="Edit"
-                type="idle"
-                size={35}
-                onClick={handleEditPalette} />
-              <Button name="Delete"
-                type="idle"
-                size={35}
-                onClick={handleDeletePalette} />
-            </>
-          }
-        </div>
-      }
+      {!editor && <Menu />}
     </Header>
   )
 }
