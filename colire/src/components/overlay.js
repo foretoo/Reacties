84c4a8fb8767f925525001b41cc983fc3adb86f7
Sergey Borovikov <serg.there@gmail.com>
2022-03-13 14:25:47 +0300

    fix: mobile overlay animation

--- colire/src/components/overlay.js
+++ colire/src/components/overlay.js
@@ -1,6 +1,6 @@
 import { h } from "preact"
 import { gsap } from "gsap"
-import { useEffect, useRef } from "preact/hooks"
+import { useLayoutEffect, useRef } from "preact/hooks"
 import "./css/overlay.css"
 
 const Overlay = ({ code, lumClass, force }) => {
@@ -9,9 +9,10 @@ const Overlay = ({ code, lumClass, force }) => {
   const mounted = useRef(false)
   const tl = gsap.timeline({ defaults: { duration: 0.5 }})
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (mounted.current) {
-      tl.to(ref.current, { opacity: "1", pointerEvents: "all" })
+      tl.set(ref.current, { background: code, pointerEvents: "all" })
+      tl.to(ref.current, { opacity: "1" })
       tl.to(ref.current, { opacity: "0" }, "+=1")
       tl.set(ref.current, { pointerEvents: "none" })
     }
@@ -22,8 +23,7 @@ const Overlay = ({ code, lumClass, force }) => {
   return (
     <section
       ref={ref}
-      className={"overlay" + lumClass}
-      style={{ background: code }} >
+      className={"overlay" + lumClass} >
       <h1>Copied</h1>
       <span>{code}</span>
     </section>
